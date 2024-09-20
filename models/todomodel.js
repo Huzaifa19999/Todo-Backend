d956c58ee214e6a3da91a25dcb798e254fc113d1
Huzaifa Abdul Qadir <90086432+Huzaifa19999@users.noreply.github.com>
2024-09-21 02:05:56 +0500

    Update todomodel.js

--- models/todomodel.js
+++ models/todomodel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const todoSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true,
+    required: false,
     trim: true,
   },
   description: {
